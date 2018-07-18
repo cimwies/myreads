96137a0ba2ee179fe8c4ac1a44cf06a1eb60beb2
Claudia <hello@allegro-online.net>
2018-07-18 23:35:33 +0200

    Refactore JS files

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -28,52 +28,53 @@ class ListBooks extends Component {
         onUpdateBookShelf: PropTypes.func.isRequired
     }
 
-
+    
     render = () => {
 
         const { books, onUpdateBookShelf } = this.props
 
-        function getBooks(shelfKey) {
-            return books.filter(book => book.shelf === shelfKey).sort(sortBy('authors'));
+        function getBooks (shelfKey) {
+            return books.filter(book => book.shelf === shelfKey)
+            .sort(sortBy('authors'))
         }
 
-    return(
-        <div className="list-books">
-            <div className="list-books-title">
-                <h1>My Reads</h1>
-            </div>
-            <div className="list-books-content">
-                <div>
-                    { shelves.map((shelf) => (
-                        <div key={shelf.key} className="bookshelf">
-                            <h2 className="bookshelf-title">{shelf.name}</h2>
-                            { getBooks(shelf.key).length === 0 ? (
-                                <div>
-                                  <h4>Sorry, no books in this shelf</h4>
-                                </div>
-                            ) : (
-                                <div className="bookshelf-books">
-                                    <ol className="books-grid">
-                                        { getBooks(shelf.key).map((book) => (
-                                            <li key = { book.id }>
-                                                <Book key = { book.title }
-                                                book = { book }
-                                                updateBookShelf = { onUpdateBookShelf }/>
-                                            </li>   
-                                          ))}
-                                    </ol>
-                                </div> 
-                            )}
-                        </div>
-                    )) }
+        return(
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>My Reads</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        { shelves.map((shelf) => (
+                            <div key={shelf.key} className="bookshelf">
+                                <h2 className="bookshelf-title">{shelf.name}</h2>
+                                { getBooks(shelf.key).length === 0 ? (
+                                    <div>
+                                      <h4>Sorry, no books in this shelf</h4>
+                                    </div>
+                                ) : (
+                                    <div className="bookshelf-books">
+                                        <ol className="books-grid">
+                                            { getBooks(shelf.key).map((book) => (
+                                                <li key = { book.id }>
+                                                    <Book key = { book.title }
+                                                    book = { book }
+                                                    updateBookShelf = { onUpdateBookShelf }/>
+                                                </li>   
+                                              ))}
+                                        </ol>
+                                    </div> 
+                                )}
+                            </div>
+                        )) }
+                    </div>
+                </div>
+                <div className="open-search">
+                    {/*Add a Book Link*/}
+                    <Link to='/search'>Add a book</Link>
                 </div>
             </div>
-            <div className="open-search">
-                {/*Add a Book Link*/}
-                <Link to='/search'>Add a book</Link>
-            </div>
-        </div>
-      );
+          );
     }
 
 }
